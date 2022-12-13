e33f626a2b0a32518cd6aa77c05ca6a0e5959162
napat103 <naphat.watt@kmutt.ac.th>
2022-12-13 22:45:40 +0700

    Finish

--- minMedMax.js
+++ minMedMax.js
@@ -1,39 +1,18 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  if (n1 < n2 && n1 < n3 && n2 < n3) {
-    return obj = {min:n1,mid:n2,max:n3}
-  }else if(n1 < n2 && n1 < n3 && n3 < n2){
-    return obj = {min:n1,mid:n3,max:n2}
-  }else if(n2 < n1 && n2 < n3 && n3 < n1){
-    return obj = {min:n2,mid:n3,max:n1}
-  }else if(n2 < n1 && n2 < n3 && n1 < n3){
-    return obj = {min:n2,mid:n1,max:n3}
-  }else if(n3 < n1 && n3 < n2 && n2 < n1){
-    return obj = {min:n3,mid:n2,max:n1}
-  }else if(n3 < n1 && n3 < n2 && n1 < n2){
-    return obj = {min:n3,mid:n1,max:n2}
-  }else if(n1 == n2 && n1 < n3 && n2 < n3){
-     return obj = {min:n1,mid:n2,max:n3}
-   }else if(n1 == n2 && n1 > n3 && n2 > n3){
-     return obj = {min:n3,mid:n1,max:n2}
-   }else if(n1 == n3 && n1 < n2 && n3 < n2){
-     return obj = {min:n1,mid:n3,max:n2}
-   }else if(n1 == n3 && n1 > n2 && n3 > n2){
-     return obj = {min:n1,mid:n2,max:n3}
-   }else if(n2 == n3 && n2 < n1 && n3 < n1){
-     return obj = {min:n2,mid:n3,max:n1}
-   }else if(n2 == n3 && n2 > n1 && n3 > n1){
-     return obj = {min:n1,mid:n2,max:n3}   
-   }else{
-     return obj = {min:n1,mid:n2,max:n3}
-   }
+  if (n1 > n2 && n2 > n3) return {max: n1, mid: n2, min: n3}
+  if (n1 > n3 && n3 > n2) return {max: n1, mid: n3, min: n2}
+  if (n1 > n2 && n2 === n3) return {max: n1, mid: n1, min: n2}
+  if (n1 > n3 && n3 === n2) return {max: n1, mid: n1, min: n3}
+  if (n2 > n1 && n1 > n3) return {max: n2, mid: n1, min: n3}
+  if (n2 > n3 && n3 > n1) return {max: n2, mid: n3, min: n1}
+  if (n2 > n1 && n1 === n3) return {max: n2, mid: n2, min: n1}
+  if (n2 > n3 && n3 === n1) return {max: n2, mid: n2, min: n3}
+  if (n3 > n1 && n1 > n2) return {max: n3, mid: n1, min: n2}
+  if (n3 > n2 && n2 > n1) return {max: n3, mid: n2, min: n1}
+  if (n3 > n1 && n1 === n2) return {max: n3, mid: n3, min: n1}
+  if (n3 > n2 && n2 === n1) return {max: n3, mid: n3, min: n2}
 }
-module.exports = minMedMax
 
-console.log(minMedMax(14,97,46))
-console.log(minMedMax(1,1,9))
-console.log(minMedMax(9,1,1))
-console.log(minMedMax(1,9,1))
-console.log(minMedMax(1,1,1))
-console.log(minMedMax(85, 75, 65))
+module.exports = minMedMax
